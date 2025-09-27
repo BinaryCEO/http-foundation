5651e6f3805f20f4249b292cc06ed14fede5ad0d
Binary CEO <92663664+BinaryCEO@users.noreply.github.com>
2025-09-27 09:35:11 +0330

    add autoload to package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'binaryceo/request',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b25decca20490c22e3300dc72f7b816423ef5b7b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'binaryceo/request' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b25decca20490c22e3300dc72f7b816423ef5b7b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
